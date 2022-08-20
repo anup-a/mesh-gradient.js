8e5980ac4ba00240fd109140cd7b538f006e0593
Anup Aglawe <aglawe.anup22@gmail.com>
2022-08-20 18:11:37 +0530

    test: improved tests

--- src/__tests__/MeshGradient.test.ts
+++ src/__tests__/MeshGradient.test.ts
@@ -1,7 +1,16 @@
 import Gradient from '../index';
+import 'jest-canvas-mock';
+import 'jest-webgl-canvas-mock'
 
 test('create new Mesh Gradient', () => {
-  console.log(Gradient)
+  const newCanvas = document.createElement('canvas');
+  newCanvas.width = 800;
+  newCanvas.height = 600;
+  newCanvas.id = "my-canvas"
+  const colors = ["#fff","#fff","#fff","#fff"]
+  document.body.appendChild(newCanvas);
+
   const meshGradient = new Gradient();
   expect(meshGradient).not.toBe(undefined);
+  expect(meshGradient.initGradient("#my-canvas", colors)).not.toBe(undefined);
 });
